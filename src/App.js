c24194986fbd7a2fdc655e17f489ab7910b90f65
RICH-CD <cardenas.ricardo94@gmail.com>
2022-10-23 20:17:35 -0500

    first test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [ditto, setDitto] = useState();
+  const [loaded, setloaded]=useState(false);
+
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then((res) => res.json())
+    .then((pokemon) => setDitto(pokemon))
+    .catch((err) => console.log(err))
+    .finally(()=> setloaded(true));
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {loaded ? ditto.name : <h2> Cargando... </h2>}
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
